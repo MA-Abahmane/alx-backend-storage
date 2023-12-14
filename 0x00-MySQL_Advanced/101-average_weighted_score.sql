e7ccb22f26c168e0d86db37d910714ff8472bfc4
Mohamed Amine <ma.abahmane@gmail.com>
2023-12-14 22:09:20 +0100

    update 101

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,9 +1,9 @@
 -- Write a SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
---
+-- Average weighted score for all
 
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+CREATE PROCEDURE `ComputeAverageWeightedScoreForUsers`()
 BEGIN
     -- Update the average_score for all users
     UPDATE users U
@@ -14,8 +14,6 @@ BEGIN
         INNER JOIN corrections C ON P.id = C.project_id
         WHERE C.user_id = U.id
     );
-END;
-
-$$
+END $$
 
 DELIMITER ;
