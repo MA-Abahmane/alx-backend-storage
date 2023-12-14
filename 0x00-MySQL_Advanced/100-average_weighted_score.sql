11ef551ac891325d858ee79b538707ae413b62bc
Mohamed Amine <ma.abahmane@gmail.com>
2023-12-14 21:59:56 +0100

    Push task 100 & 101

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,25 +1,26 @@
 
 -- Write a SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
--- Compute average weighted score
+-- CALCULATE the average weighted score
 
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
-DELIMITER //
+DROP PROCEDURE IF EXISTS `ComputeAverageWeightedScoreForUser`;
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
+DELIMITER $$
+
+CREATE PROCEDURE `ComputeAverageWeightedScoreForUser` (
     p_user_id INT
 )
 BEGIN
-    DECLARE w_avg_score FLOAT;
+    DECLARE wgt_avg_score FLOAT;
 
     -- Calculate weighted average score
     SELECT SUM(C.score * P.weight) / SUM(P.weight)
-    INTO w_avg_score
+    INTO wgt_avg_score
     FROM corrections AS C
     JOIN projects AS P ON C.project_id = P.id
     WHERE C.user_id = p_user_id;
 
     -- Update average_score for the user
-    UPDATE users SET average_score = w_avg_score WHERE id = p_user_id;
-END //
+    UPDATE users SET average_score = wgt_avg_score WHERE id = p_user_id;
+END $$
 
 DELIMITER ;
---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,21 @@
+-- Write a SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
+-- Average weighted score for all
+
+DROP PROCEDURE IF EXISTS `ComputeAverageWeightedScoreForUsers`
+
+DELIMITER $$
+
+CREATE PROCEDURE `ComputeAverageWeightedScoreForUsers` ()
+BEGIN
+    -- Update the average_score for all users
+    UPDATE users U
+    SET U.average_score = (
+        -- Calculate the weighted average score for each user
+        SELECT SUM(P.weight * C.score) / SUM(P.weight)
+        FROM projects P
+        INNER JOIN corrections C ON P.id = C.project_id
+        WHERE C.user_id = U.id
+    );
+END $$
+
+DELIMITER ;
