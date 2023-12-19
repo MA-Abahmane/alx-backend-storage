f94fe25ba22c75c219f4b525f536b1a892b3a837
MAA <MA.abahmane@gmail.com>
2023-12-19 03:44:19 -0800

    update task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,7 +8,7 @@ Write a Python script that provides some stats about Nginx logs stored in
     * Display (same as the example):
     * first line: x logs where x is the number of documents in this collection
     * second line: Methods:
-    * 5 lines with the number of documents with the method = ["GET", "POST", "PUT", "PATCH", "DELETE"] in this order (see example below - warning: it’s a tabulation before each line)
+    * 5 lines with the number of documents with the method = ["GET", "POST", "PUT", "PATCH", "DELETE"] in this order (se        e example below - warning: it’s a tabulation before each line)
     * one line with the number of documents with:
     * method=GET
     * path=/status
