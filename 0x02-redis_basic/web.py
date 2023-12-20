0d1ddbbe4d5dc0e8ac00b24c943dbc28630389da
Mohamed Amine <ma.abahmane@gmail.com>
2023-12-20 22:11:07 +0100

    Update web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,19 +1,16 @@
-import requests
+#!/usr/bin/env python3
+
+"""
+Implementing an expiring web cache and tracker
+"""
 from functools import lru_cache
-import time
+import requests
 
-@lru_cache(maxsize=None, typed=False)
+@lru_cache(maxsize=128, typed=False)
 def get_page(url: str) -> str:
-    # Track the access count for the URL
-    get_page.access_count[url] = get_page.access_count.get(url, 0) + 1
-    print(f"Access count for {url}: {get_page.access_count[url]}")
-
-    # Simulate a slow response
-    time.sleep(5)
-
-    # Make the request and return the HTML content
+    """ 
+        uses the requests module to obtain the HTML content of
+        a particular URL and returns it
+    """
     response = requests.get(url)
     return response.text
-
-# Dictionary to store the access count for each URL
-get_page.access_count = {}
