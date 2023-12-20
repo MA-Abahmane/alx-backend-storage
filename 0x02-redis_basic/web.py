a895c80d55e2d60cf2d33dc691a3b68689e2f1cf
Mohamed Amine <ma.abahmane@gmail.com>
2023-12-20 22:23:56 +0100

    Update web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,12 +3,14 @@
 """
 Implementing an expiring web cache and tracker
 """
+
 from functools import lru_cache
 import requests
 
+
 @lru_cache(maxsize=128, typed=False)
 def get_page(url: str) -> str:
-    """ 
+    """
         uses the requests module to obtain the HTML content of
         a particular URL and returns it
     """
