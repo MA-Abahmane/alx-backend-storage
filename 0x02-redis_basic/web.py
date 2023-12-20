a4c15c5d5cef3850e1a7a3980c8e524a6a32a415
Mohamed Amine <ma.abahmane@gmail.com>
2023-12-20 21:41:02 +0100

    Update web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,12 +3,45 @@
 """
 Implementing an expiring web cache and tracker
 """
-import time
+
+import redis
 import requests as req
+from typing import Callable
 from functools import lru_cache
 
 
-@lru_cache(maxsize=128, typed=False)
+# Redis database connection
+db = redis.Redis()
+
+def Count_calls(func: Callable) -> Callable:
+    """
+        Decorator to count how many times a function is called.
+        Increments the count in the Redis database.
+        Caches the page content with a 10-second expiration time.
+    """
+    def wrapper(url: str) -> str:
+        """
+            Wrapper function that tracks the number
+            of calls and caches the result.
+        """
+        # Increment the count in Redis
+        db.incr(f'count:{url}')
+        # Check if the page content is already cached
+        cachedPage = db.get(f'cached:{url}')
+
+        # Fetch the page content using the provided function
+        if cachedPage:
+            return cachedPage.decode('UTF-8')
+
+        # Cache the page content with a 10-second expiration time
+        response = func(url)
+        db.setex(f'cashed:{url}', 10, response)
+
+        return response
+    return wrapper
+
+
+@Count_calls
 def get_page(url: str) -> str:
     """
         uses the requests module to obtain the HTML content of
